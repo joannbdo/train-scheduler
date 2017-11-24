26985a6693c660ed86a26c5961b60401f81e9955
joannbdo <joannbdo@gmail.com>
2017-11-24 12:04:41 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+var config = {
+  apiKey: "AIzaSyDuj8MdE72ljr8bP0Sx38CnaQ0nHBhdMtw",
+  authDomain: "st-project-729ab.firebaseapp.com",
+  databaseURL: "https://st-project-729ab.firebaseio.com",
+  projectId: "st-project-729ab",
+  storageBucket: "st-project-729ab.appspot.com",
+  messagingSenderId: "791886103331"
+  };
+  firebase.initializeApp(config);
+
+// VARIABLES
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+
+
+// FUNCTIONS + EVENTS
+$("#addTrain").on("click", function() {
+
+  trainName = $('#nameInput').val().trim();
+  destination = $('#destinationInput').val().trim();
+  firstTrainTime = $('#firstTrainInput').val().trim();
+  frequency = $('#frequencyInput').val().trim();
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  });
+
+    return false;
+});
+
+
+// MAIN PROCESS + INITIAL CODE
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  // update the variable with data from the database
+  trainName = snapshot.val().trainName;
+  destination = snapshot.val().destination;
+  firstTrainTime = snapshot.val().firstTrainTime;
+  frequency = snapshot.val().frequency;
+
+
+  
+  var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
+  var nowMoment = moment(); 
+
+  var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+  var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+  var minutesAway = frequency - minutesSinceLastArrival;
+
+  var nextArrival = nowMoment.add(minutesAway, 'minutes');
+  var formatNextArrival = nextArrival.format("HH:mm");
+
+
+  // add table
+  var tr = $('<tr>');
+  var a = $('<td>');
+  var b = $('<td>');
+  var c = $('<td>');
+  var d = $('<td>');
+  var e = $('<td>');
+  a.append(trainName);
+  b.append(destination);
+  c.append(frequency);
+  d.append(formatNextArrival);
+  e.append(minutesAway);
+  tr.append(a).append(b).append(c).append(d).append(e);
+  $('#newTrains').append(tr);
+
+
+  }, function (errorObject) {
+
+  // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+
+});
